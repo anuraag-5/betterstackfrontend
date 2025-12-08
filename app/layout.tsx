06ffc0a1fca386baece812f3d24b578883a07bb2
Anurag bhoite <anuragcr7778@gmail.com>
2025-12-08 21:47:11 +0530

    Completing Landing Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { Toaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
-import { delaFont, neueFont, poppinsFont } from "@/app/fonts/fonts";
+import { poppinsFont } from "@/app/fonts/fonts";
 import "./globals.css";
 
 export const metadata: Metadata = {
