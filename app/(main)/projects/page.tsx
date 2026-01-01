98af70cb62f774f08d090d52553408f240dcc3cc
Anurag bhoite <anuragcr7778@gmail.com>
2026-01-01 18:28:44 +0530

    Cleaned up

--- app/(main)/projects/page.tsx
+++ app/(main)/projects/page.tsx
@@ -29,38 +29,49 @@ const YourProjects = () => {
         </div>
       </div>
       <div className="flex-1 bg-[#262626] mt-6 rounded-4xl md:rounded-tl-4xl md:rounded-bl-4xl md:rounded-tr-[0px] md:rounded-br-[0px] p-6 grid 2xl:grid-cols-3 xl:grid-cols-2 grid-cols-1 gap-y-16 place-items-center md:place-items-start overflow-y-auto">
-        {websites?.map((w, i) => (
-          <div
-            key={w.domain}
-            className="flex flex-col h-[380px] w-fit border border-[#767676] rounded-4xl cursor-pointer"
-            onClick={() => handleProjectClick(w.domain)}
-          >
+        {websites?.length === 0 || !websites ? (
+          <div className="flex flex-col gap-4 justify-start text-[#A8A8A8] w-full h-full py-3">
+            <p className="text-xl md:text-2xl">You have no projects</p>
+            <p className="text-sm text-[#C499FF]">
+              Click <span className="text-white font-medium mx-1">Add +</span> to
+              create your first project
+            </p>
+          </div>
+        ) : (
+          websites.map((w, i) => (
             <div
-              className={
-                linearGradients[i % 3] + " flex-1 rounded-tl-4xl rounded-tr-4xl"
-              }
-            ></div>
-            <div className="flex gap-4 items-center px-5 py-7 border-t border-[#767676]">
-              <Image
-                src="/images/violet-project-icon.svg"
-                alt=""
-                width={26}
-                height={26}
-              />
-              <div>
-                <div>{w.domain}</div>
-                <div
-                  className={
-                    neueFont.className +
-                    " lg:text-sm text-[12px] text-[#A8A8A8] min-w-[250px] md:min-w-[295px]"
-                  }
-                >
-                  {w.about}
+              key={w.domain}
+              className="flex flex-col h-[380px] w-fit border border-[#767676] rounded-4xl cursor-pointer"
+              onClick={() => handleProjectClick(w.domain)}
+            >
+              <div
+                className={
+                  linearGradients[i % 3] +
+                  " flex-1 rounded-tl-4xl rounded-tr-4xl"
+                }
+              ></div>
+              <div className="flex gap-4 items-center px-5 py-7 border-t border-[#767676]">
+                <Image
+                  src="/images/violet-project-icon.svg"
+                  alt=""
+                  width={26}
+                  height={26}
+                />
+                <div>
+                  <div>{w.domain}</div>
+                  <div
+                    className={
+                      neueFont.className +
+                      " lg:text-sm text-[12px] text-[#A8A8A8] min-w-[250px] md:min-w-[295px]"
+                    }
+                  >
+                    {w.about}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
