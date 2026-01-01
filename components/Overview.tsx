fabb7f3129b50d5f83c600058778d8acfeecbd50
Anurag bhoite <anuragcr7778@gmail.com>
2026-01-01 19:07:25 +0530

    Cleaned Up

--- components/Overview.tsx
+++ components/Overview.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as motion from "motion/react-client";
 import Image from "next/image";
 import { Analytics, regions } from "@/lib/types";
 import { useEffect, useState } from "react";
@@ -13,10 +14,11 @@ import {
 } from "@/lib/websiteFunctions";
 
 const Overview = ({ domain }: { domain: string }) => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
   const [totalViews, setTotalViews] = useState(0);
   const [avgRespTime, setAvgRespTime] = useState(0);
   const [uniqueUsers, setUniqueUsers] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
   const [uptimePercent, setUptimePercent] = useState(0);
   const [selectedRegion, setSelectedRegion] = useState("All Regions");
   const [combinedUptime, setCombinedUptime] = useState(0);
@@ -28,10 +30,12 @@ const Overview = ({ domain }: { domain: string }) => {
   const toggleDropdownOpen = () => setDropdownOpen((iv) => !iv);
   const toggleSelectedRegionType = (e: React.MouseEvent<HTMLDivElement>) => {
     const newRegion = e.currentTarget.dataset.id!;
-    const newRegionData = analyticByRegions?.analyticByRegions.find((data) => newRegion === data.region) ?? {
+    const newRegionData = analyticByRegions?.analyticByRegions.find(
+      (data) => newRegion === data.region
+    ) ?? {
       avgResp: combinedAvgRespTime,
       uptime: combinedUptime,
-      region: newRegion
+      region: newRegion,
     };
     setAvgRespTime(newRegionData!.avgResp);
     setUptimePercent(newRegionData!.uptime);
@@ -80,7 +84,7 @@ const Overview = ({ domain }: { domain: string }) => {
         analyticByRegions: analyticByRegionsData,
       });
     };
-    getAllData();
+    getAllData().finally(() => setLoading(false));
   }, [domain]);
   return (
     <div className="w-fit">
@@ -128,65 +132,147 @@ const Overview = ({ domain }: { domain: string }) => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col xl:flex-row my-7 gap-8">
-          <div className="min-w-[275px] md:min-w-[300px] flex flex-col items-center border-2 border-[#767676] rounded-2xl py-5">
-            <Image src="/images/time-white.svg" alt="" width={40} height={40} />
-            <div className="text-md md:text-lg pt-3">Avg Rsp Time</div>
-            <div className="text-xl md:text-3xl text-[#C499FF] mt-2">
-              {
-                selectedRegion === "All Regions" ? Math.round(combinedAvgRespTime) : Math.round(avgRespTime)
-              } ms
+        {!loading ? (
+          <>
+            <div className="flex flex-col xl:flex-row my-7 gap-8">
+              <div className="min-w-[275px] md:min-w-[300px] flex flex-col items-center border-2 border-[#767676] rounded-2xl py-5">
+                <Image
+                  src="/images/time-white.svg"
+                  alt=""
+                  width={40}
+                  height={40}
+                />
+                <div className="text-md md:text-lg pt-3">Avg Rsp Time</div>
+                <div className="text-xl md:text-3xl text-[#C499FF] mt-2">
+                  {selectedRegion === "All Regions"
+                    ? Math.round(combinedAvgRespTime)
+                    : Math.round(avgRespTime)}{" "}
+                  ms
+                </div>
+              </div>
+              <div className="min-w-[275px] md:min-w-[300px] flex flex-col items-center border-2 border-[#767676] rounded-2xl py-5">
+                <Image
+                  src="/images/unique-views.svg"
+                  alt=""
+                  width={46}
+                  height={46}
+                />
+                <div className="text-md md:text-lg mt-2">Uptime</div>
+                <div className="text-xl md:text-3xl text-[#C499FF] mt-2">
+                  {selectedRegion === "All Regions"
+                    ? Math.round(combinedUptime)
+                    : Math.round(uptimePercent)}
+                  %
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="min-w-[275px] md:min-w-[300px] flex flex-col items-center border-2 border-[#767676] rounded-2xl py-5">
-            <Image
-              src="/images/unique-views.svg"
-              alt=""
-              width={46}
-              height={46}
-            />
-            <div className="text-md md:text-lg mt-2">Uptime</div>
-            <div className="text-xl md:text-3xl text-[#C499FF] mt-2">
-              {
-                selectedRegion === "All Regions" ? Math.round(combinedUptime) : Math.round(uptimePercent)
-              }%
+          </>
+        ) : (
+          <>
+            <div className="flex flex-col xl:flex-row my-7 gap-8">
+              <motion.div
+                className="h-[200px] min-w-[275px] md:min-w-[300px] bg-[#767676] rounded-2xl"
+                initial={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: 1,
+                }}
+                transition={{
+                  duration: 0.4,
+                  repeat: Infinity,
+                  repeatType: "reverse",
+                }}
+              ></motion.div>
+              <motion.div
+                className="h-[170px] min-w-[275px] md:min-w-[300px] bg-[#767676] rounded-2xl"
+                initial={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: 1,
+                }}
+                transition={{
+                  duration: 0.4,
+                  repeat: Infinity,
+                  repeatType: "reverse",
+                }}
+              ></motion.div>
             </div>
-          </div>
-        </div>
+          </>
+        )}
       </div>
       <div>
         <div className="bg-clip-text text-transparent bg-gradient-to-r from-[#ffffff] to-[#999999] inline-block text-xl">
           Popularity :
         </div>
-        <div className="flex flex-col xl:flex-row my-7 gap-8">
-          <div className="min-w-[275px] md:min-w-[300px] flex flex-col items-center border-2 border-[#767676] rounded-2xl py-5">
-            <Image
-              src="/images/total-views.svg"
-              alt=""
-              width={50}
-              height={50}
-            />
-            <div className="text-md md:text-lg">Total Views</div>
-            <div className="text-xl md:text-3xl text-[#C499FF] mt-2">
-              {totalViews}
+        {!loading ? (
+          <>
+            <div className="flex flex-col xl:flex-row my-7 gap-8">
+              <div className="min-w-[275px] md:min-w-[300px] flex flex-col items-center border-2 border-[#767676] rounded-2xl py-5">
+                <Image
+                  src="/images/total-views.svg"
+                  alt=""
+                  width={50}
+                  height={50}
+                />
+                <div className="text-md md:text-lg">Total Views</div>
+                <div className="text-xl md:text-3xl text-[#C499FF] mt-2">
+                  {totalViews}
+                </div>
+              </div>
+              <div className="min-w-[275px] md:min-w-[300px] flex flex-col items-center border-2 border-[#767676] rounded-2xl py-5">
+                <Image
+                  src="/images/unique-views.svg"
+                  alt=""
+                  width={46}
+                  height={46}
+                />
+                <div className="text-md md:text-lg mt-2">Unique Users</div>
+                <div className="text-xl md:text-3xl text-[#C499FF] mt-2">
+                  {uniqueUsers}
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="min-w-[275px] md:min-w-[300px] flex flex-col items-center border-2 border-[#767676] rounded-2xl py-5">
-            <Image
-              src="/images/unique-views.svg"
-              alt=""
-              width={46}
-              height={46}
-            />
-            <div className="text-md md:text-lg mt-2">Unique Users</div>
-            <div className="text-xl md:text-3xl text-[#C499FF] mt-2">
-              {uniqueUsers}
+          </>
+        ) : (
+          <>
+            <div className="flex flex-col xl:flex-row my-7 gap-8">
+              <motion.div
+                className="h-[170px] min-w-[275px] md:min-w-[300px] bg-[#767676] rounded-2xl"
+                initial={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: 1,
+                }}
+                transition={{
+                  duration: 0.4,
+                  repeat: Infinity,
+                  repeatType: "reverse",
+                }}
+              ></motion.div>
+              <motion.div
+                className="h-[180px] min-w-[275px] md:min-w-[300px] bg-[#767676] rounded-2xl"
+                initial={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: 1,
+                }}
+                transition={{
+                  duration: 0.4,
+                  repeat: Infinity,
+                  repeatType: "reverse",
+                }}
+              ></motion.div>
             </div>
-          </div>
-        </div>
+          </>
+        )}
       </div>
     </div>
   );
 };
 
 export default Overview;
+export const dynamic = "force-dynamic";
